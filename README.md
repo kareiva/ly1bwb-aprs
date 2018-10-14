e19cf2717639c0688151b85b2ee51b1366e923de
Simonas Kareiva <simonas@5grupe.lt>
2018-10-14 16:19:30 +0300

    initial ly1bwb commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LY1BWB clubstation APRS iGate
+
+This is a sample configuration which should work with APRX and ax25-tools
---
+++ kiss.sh
@@ -0,0 +1,2 @@
+/usr/sbin/kissattach -m 512 /dev/ttyUSB0 aprs 44.128.0.1
+/usr/sbin/kissparms -p aprs -t 300 -r 128 -s 100 -l 30
